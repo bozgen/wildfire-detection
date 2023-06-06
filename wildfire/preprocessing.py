8de07d173edc8cab45ab6d1832629d4e361cdfc9
bozgen <bilalozgen00@gmail.com>
2023-06-06 11:32:01 +0300

    prepare images before preprocessing

--- wildfire/preprocessing.py
+++ wildfire/preprocessing.py
@@ -16,18 +16,24 @@ def resize_in_place():
 		resized = tf.image.resize(image, (250,250)).numpy()
 		cv2.imwrite(os.path.join(datadir, f'{idx}.jpg'), resized)
 
+def prepare_images():
+	rename_in_place()
+	resize_in_place()
+
 def mass_flip(axis):
 	"""flips the image in the desired axis
 
 	Args:
 		axis (_int_): 0 for vertical, 1 for horizontal
 	"""
+	prepare_images()
 	for idx, img in enumerate(os.listdir(datadir)):
 		image = cv2.imread(os.path.join(datadir,f'{idx}.jpg'))
 		flipped = cv2.flip(image, axis)
 		cv2.imwrite(os.path.join(datadir, f'flipped{axis}{idx}.jpg'), flipped)
 
 def contrast():
+	prepare_images()
 	for idx, image in enumerate(os.listdir(datadir)):
 		img = cv2.imread(os.path.join(datadir, f'{idx}.jpg'), 1)
 		lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
@@ -50,6 +56,5 @@ def contrast():
 def main():
 	contrast()
 	
-
 
 main()
