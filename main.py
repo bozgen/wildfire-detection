bae3ed2644205ff24c2bc24dd6b39ad07230c649
bozgen <bilalozgen00@gmail.com>
2023-06-06 11:32:21 +0300

    add new example

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from wildfire.wildfire import Model
+from wildfire.preprocessing import contrast, mass_flip
 
+
+# PREPROCESSING
+# mass_flip(1)
+
+
+# PREDICTION
 model = Model()
 # model.train(epochs=10)
 model.load_weights()
 model.evaluate()
-model.test_and_show_results()
+model.test_and_show_first_ten()
